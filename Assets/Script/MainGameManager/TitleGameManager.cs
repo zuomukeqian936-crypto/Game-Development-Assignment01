352f890ee5ad3b6039af395e4dd683e73d72c320
zuomukeqian936-crypto <zuomukeqian936@gmail.com>
2026-07-04 14:22:30 +0900

    タイトル画面のボタンアニメーションの追加

---
+++ Assets/Script/MainGameManager/TitleGameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// タイトルUI設定
+/// </summary>
+public class TitleGameManager : MonoBehaviour
+{
+    [Header("UI Settings")]
+    [SerializeField] private GameObject _gamePlayButton;
+    [SerializeField] private Text _titleText;
+}
